b91b6694dc6ad274ce041357f87b8332053e9daa
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-11-01 17:12:12 -0600

    messing with email field

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, EmailField
+# from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import User
 
